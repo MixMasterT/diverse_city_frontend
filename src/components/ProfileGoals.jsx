ab2a92dee9850ccd44eda54e0bfa2550d8585602
Katie Mathews <ktmathews89@gmail.com>
2018-10-14 06:42:33 -0500

    style profile goals

--- src/components/ProfileGoals.jsx
+++ src/components/ProfileGoals.jsx
@@ -2,37 +2,61 @@ import React from "react";
 import PropTypes from "prop-types";
 import {Link} from 'react-router-dom';
 
-const ProfileGoals = ({ user }) =>
-  user ? (
-    <div className="profile-goals">
-      <div className="text-center">
-        <i className="fa fa-th-list fa-5x" />
-        <h3>Goals</h3>
-      </div>
-      {user.goals && user.goals.length > 0 ? (
-        <ul>
-          {user.goals.map(goal => (
-            <div key={goal._id} className="list-group">
-              <h5>{goal.name}</h5>
-              <p>{goal.description}</p>
+class ProfileGoals extends React.Component {
+  componentDidMount() {
+      this.props.loadUser();
+  }
+
+  render () {
+    var buildGoalLink = (userId, goalId) => {
+      return `goal/${userId}/${goalId}`;
+    };
+    var milestonesComplete = goal => {
+        return goal.milestones.reduce((accum, value, index) => {
+          if (value.completed){
+              return (accum + 1);
+          }
+          return accum;
+        }, 0);
+    };
+
+    return (
+      this.props.user ? (
+        <div className="profile-goals">
+          <div className="text-center">
+            <i className="fa fa-th-list fa-5x" />
+            <h3>Goals</h3>
+          </div>
+          {this.props.user.goals && this.props.user.goals.length > 0 ? (
+            <div className="list-group">
+              {this.props.user.goals.map(goal => (
+                <Link key={goal.g_id} className="list-group-item goal-link" to={buildGoalLink(this.props.user.phone, goal.g_id)}>
+                  <div>{goal.name}</div>
+                    <div className="progress">
+                      <div className="progress-bar" role="progressbar" aria-valuenow={milestonesComplete(goal)} aria-valuemin="0" aria-valuemax={goal.milestones.length}></div>
+                    </div>
+                </Link>
+              ))}
             </div>
-          ))}
-        </ul>
+          ) : (
+            <span>
+              No Goals yet. Please check them out here <Link to="browse">here</Link>
+            </span>
+          )}
+        </div>
       ) : (
         <span>
-          No Goals yet. Please check them out here <Link to="browse">here</Link>
+          Please Login <Link to="login">here</Link>
         </span>
-      )}
-    </div>
-  ) : (
-    <span>
-      Please Login <Link href="login">here</Link>
-    </span>
-  );
+      )
+    );
+  }
+}
+
 
 ProfileGoals.propTypes = {
   user: PropTypes.object,
-  loadUserGoals: PropTypes.func
+  loadUser: PropTypes.func
 };
 
 export default ProfileGoals;
--- src/containers/ProfileGoals.js
+++ src/containers/ProfileGoals.js
@@ -10,7 +10,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  loadUserGoals: () => dispatch(fetchUser())
+  loadUser: () => dispatch(fetchUser()),
 });
 
 export default connect(
