c0bc810e428757e440ed839fa593e31d9da4901f
MixMasterT <torahlight@gmail.com>
2018-10-14 02:18:15 -0500

    Add build directory for deployment

---
+++ build/precache-manifest.34b9913c6543857f3f451b7aa3233397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "08dcdd9598369a68df72",
+    "url": "/static/js/main.08dcdd95.chunk.js"
+  },
+  {
+    "revision": "63f8446636460aa2014f",
+    "url": "/static/js/1.63f84466.chunk.js"
+  },
+  {
+    "revision": "08dcdd9598369a68df72",
+    "url": "/static/css/main.ef0194c5.chunk.css"
+  },
+  {
+    "revision": "63f8446636460aa2014f",
+    "url": "/static/css/1.8e6e1de3.chunk.css"
+  },
+  {
+    "revision": "068e2c142f9973a6925937f3e15ee345",
+    "url": "/index.html"
+  }
+];
