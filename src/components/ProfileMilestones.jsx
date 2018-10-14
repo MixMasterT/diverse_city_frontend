501bc7049c9cb80c193f0d8745b3c5a94c5600f5
farrellw <farrell.will.g@gmail.com>
2018-10-13 20:29:51 -0500

    Display goals on a user, not milestones.

--- src/components/ProfileMilestones.jsx
+++ src/components/ProfileMilestones.jsx
@@ -1,34 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const ProfileMilestones = ({ user }) => (
-  (user) ? ( 
+const ProfileMilestones = ({ user }) =>
+  user ? (
     <div className="profile-milestones-view">
-    <div className="text-center">
-      <i className="fa fa-th-list fa-3x"></i>
-    </div>
-    { (user.milestones && user.milestones.length > 0) ? 
-      (
-        <ul >
-        {
-          user.milestones.map((milestone) => (
-            <div key={milestone.id} className="list-group">
-              <h5>{milestone.name}</h5>
-              <p>{milestone.description}</p>
+      <div className="text-center">
+        <i className="fa fa-th-list fa-3x" />
+      </div>
+      {user.goals && user.goals.length > 0 ? (
+        <ul>
+          {user.goals.map(goal => (
+            <div key={goal.id} className="list-group">
+              <h5>{goal.name}</h5>
+              <p>{goal.description}</p>
             </div>
-          ))
-        }
+          ))}
         </ul>
       ) : (
-      <span>No Milestones yet. Please add one <a href="add-milestone">here</a></span>
-    )
-    }
-    
-  </div>
+        <span>
+          No Goals yet. Please check them out here <a href="goals">here</a>
+        </span>
+      )}
+    </div>
   ) : (
-    <span>Please Login <a href="login">here</a></span>
-  ) 
-);
+    <span>
+      Please Login <a href="login">here</a>
+    </span>
+  );
 
 ProfileMilestones.propTypes = {
   milestones: PropTypes.arrayOf(PropTypes.object)
